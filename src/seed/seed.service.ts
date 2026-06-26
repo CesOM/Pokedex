db9d9d9f175b8adc274fdd13a047d0c4f36b4586
CesOM <Cesarorozcomejia6@gmail.com>
2026-06-25 22:09:13 -0500

    antes tarea

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -9,7 +9,13 @@ export class SeedService {
   constructor(private readonly http: HttpService) {}
 
   async executeSeed() {
-    const { data } = await firstValueFrom(this.http.get('https://pokeapi.co/api/v2/pokemon?limit=650'));
+    const { data } = await firstValueFrom(this.http.get('https://pokeapi.co/api/v2/pokemon?limit=1'));
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      console.log(segments);
+      const no = +segments[segments.length - 2];
+      console.log(name, no);
+    });
     return data;
   }
 }
